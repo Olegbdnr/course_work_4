9fd47c0c76b172e86482c4eec6c5758ef9fee5e0
Olegbdnr <oleg.br.1304@gmail.com>
2026-02-02 15:38:58 +0200

    chore: init spring boot project

---
+++ soportua/src/main/java/com/lviv/iot/soportua/SoportuaApplication.java
@@ -0,0 +1,13 @@
+package com.lviv.iot.soportua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoportuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoportuaApplication.class, args);
+	}
+
+}
---
+++ soportua/src/test/java/com/lviv/iot/soportua/SoportuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lviv.iot.soportua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoportuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
